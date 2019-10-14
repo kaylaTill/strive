b9187527b1bda872e1258c62535238efa85f90e5
kaylaTill <kaylatillman319@gmail.com>
2019-10-14 00:40:28 -0700

    FrontEnd: Working Register Form Working NavBar RootHome Page Text Completed Next: - Success Register Form Page - Fix alerts - Move register form => on click, render server get route /register - CSS for register form - Complete login form - Login => render userHomePage
    BackEnd:
    Database Populated with quotes
    Database Connected with Sequelize
    Working get route for quotes
    Working post route for user registration
    Next:
    - get req register form => render registration form
    - /login route
    - add session to app

--- strive_backend/routes/register.js
+++ strive_backend/routes/register.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
 const users = require('../models/users.js');
 
 
@@ -10,20 +9,6 @@ const users = require('../models/users.js');
 // });
 
 
-// //register
-// router.post('/register', function (req, res, next) {
-//     console.log(request.body);
-//     // users.User.create({
-//     //     first_name: request.body.first_name,
-//     //     last_name: request.body.last_name,
-//     //     email: request.body.email,
-//     //     username: request.body.username,
-//     //     password: request.body.password
-//     // })
-//     //     .then(() => {
-//     //         res.sendStatus(201).end()
-//     //     });
-// });
 
 
 module.exports = router;
