3764c05a0de2774cfbab48222f7b07dac4a806f1
kaylaTill <kaylatillman319@gmail.com>
2019-10-13 23:57:36 -0700

    Added Register Form Rendering to Screen Saving Input and Printing Characters to Screen Post Req. not setup

---
+++ strive_frontend/src/register.js
@@ -0,0 +1,126 @@
+import React from "react";
+import { HelpBlock, FormGroup, FormControl, ControlLabel, Form, Button} from "react-bootstrap";
+import axios from 'axios';
+
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            first_name: "",
+            last_name: "",
+            username: "",
+            password: "",
+            confirmPassword: "",
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    validForm() {
+        return (
+            this.state.email.length > 0 &&
+            this.state.first_name.length > 0 &&
+            this.state.last_name.length > 0 &&
+            this.state.username.length > 0 &&
+            this.state.password.length > 0 &&
+            this.state.password === this.state.confirmPassword
+        );
+    }
+
+    async handleSubmit(event) {
+        event.preventDefault();
+
+        if (!validForm()) {
+            alert('Sorry not a valid form!')
+        }
+
+        this.props.handleRegister(
+            this.state.email,
+            this.state.first_name,
+            this.state.last_name,
+            this.state.username,
+            this.state.password
+        )
+    }
+
+
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} >
+                <FormGroup controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <FormControl
+                        autoFocus
+                        type="email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+                <FormGroup controlId="first_name">
+                    <Form.Label>First Name</Form.Label>
+                    <FormControl
+                        autoFocus
+                        type="first_name"
+                        name="first_name"
+                        value={this.state.first_name}
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+
+                <FormGroup controlId="last_name">
+                    <Form.Label>Last Name</Form.Label>
+                    <FormControl
+                        type="last_name"
+                        name="last_name"
+                        value={this.state.last_name}
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+
+                <FormGroup controlId="username">
+                    <Form.Label>Username</Form.Label>
+                    <FormControl
+                        type="username"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+
+                <FormGroup controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <FormControl
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+
+                <FormGroup controlId="confirmPassword">
+                    <Form.Label>Conform Password</Form.Label>
+                    <FormControl
+                        type="password"
+                        name="confirmPassword"
+                        value={this.state.confirmPassword}
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+                <Button type="submit">Submit</Button>
+            </form> 
+        );
+    }
+}
+
+
+export default Register;
--- strive_frontend/src/rootHome.js
+++ strive_frontend/src/rootHome.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import NavBar from './navbar.js';
 import styled from 'styled-components';
+import Register from './register.js'
+import axios from 'axios';
 
 const Center = styled.h1`
     width: 1000px; 
@@ -12,14 +14,41 @@ const Center = styled.h1`
     font-family: OCR A Std, monospace;
 `;
 
+class RootHome extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleRegister = this.handleRegister.bind(this);
+    }
 
-const RootHome = ((props) => {
-    return (
-        <div>
-            <NavBar/>
-            <Center>"Not your average to-do list"</Center> 
-        </div>
-    );
-})
+
+    handleRegister(email, first_name, last_name, username, password) {
+        axios.post('/user', {
+            email: email,
+            first_name: first_name,
+            last_name: last_name,
+            username: username,
+            password: password,
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <Center>"Not your average to-do list"</Center>
+                <Register handleRegister={this.handleRegister}/>
+            </div>
+        );
+    }
+
+
+}
 
 export default RootHome;
