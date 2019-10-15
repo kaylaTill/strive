968c1d920cd447c26366437168263eda0b24b0bc
kaylaTill <kaylatillman319@gmail.com>
2019-10-15 16:04:22 -0700

    User Login and Home Page Home Page Rendering on Correct Login Home Page Styled Home Page Navbar Rendering Correctly and Styled

--- strive_frontend/src/userHomepage.js
+++ strive_frontend/src/userHomepage.js
@@ -1,6 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Suspense } from 'react';
+import styled from 'styled-components';
+import { Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Link, Route, Switch, Redirect } from 'react-router-dom';
 import axios from 'axios';
+
+
+
+const Center = styled.h1`
+    width: 1000px; 
+    margin: auto 0;
+    padding: 60px;
+    align: center;
+    text-align: center;
+    font-size: 50px;
+    font-family: OCR A Std, monospace;
+`;
+
+const Nav = styled.div`
+    max-width: 1010px;
+    padding: 26px 20px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    background-color: #fff;
+`;
+const NavLeft = styled.div`
+    width: 20%;
+    text-align: left;
+    color: rgba(0, 0, 0, 1);
+    font-family: OCR A Std, monospace; 
+    text-decoration: none; 
+`;
+
+const NavRight = styled.div`
+    width: 50%;
+    text-align: right;
+    position: relative;
+    left: 45%;
+    svg {
+        margin-right: 50px;
+    }
+`;
+const NavItem = styled.div`
+    color: rgba(0, 0, 0, 1);
+    float: left;
+    padding: 12px;
+    text-decoration: none;
+    text-align: right; 
+    font-size: 15px;
+    font-family: OCR A Std, monospace;
+`;
 
 class UserHomePage extends React.Component {
     constructor(props) {
@@ -27,10 +77,41 @@ class UserHomePage extends React.Component {
     render() {
         return (
             <div>
-                <div>{`${this.state.quote.quote_text}`}</div>
-                <div>{`- ${this.state.quote.quote_author}`}</div>
-            </div>
+                <Nav>
+                    <NavLeft>
+                        <Link to={'/home'}>
+                            <NavItem> Strive </NavItem>
+                        </Link>
+                    </NavLeft>
 
+                    <NavRight>
+                        <Link to={'/objectives'}>
+                            <NavItem> Objectives </NavItem>
+                        </Link>
+                        <NavItem>|</NavItem>
+
+                        <Link to={'/keyResults'}>
+                            <NavItem> Key Results </NavItem>
+                        </Link>
+                        <NavItem>|</NavItem>
+
+                        <Link to={'/progress'}>
+                            <NavItem> Progress </NavItem>
+                        </Link>
+                        <NavItem>|</NavItem>
+
+                        <Link to={'/'}>
+                            <NavItem> Logout </NavItem>
+                        </Link>
+                    </NavRight>
+                </Nav>
+
+                <Center>
+                    <div>{`${this.state.quote.quote_text}`}</div>
+                    <br></br>
+                    <div>{`- ${this.state.quote.quote_author}`}</div>
+                </Center>
+            </div>
         );
     }
 }
