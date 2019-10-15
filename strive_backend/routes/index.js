e9c453fa63b129ae29bc4aca9c54bf291255962d
kaylaTill <kaylatillman319@gmail.com>
2019-10-15 14:42:15 -0700

    Added /welcome prefix to Home Routes

--- strive_backend/routes/index.js
+++ strive_backend/routes/index.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 /* GET home page. */
-router.get('*', function(req, res, next) {
+router.get('/*', function(req, res, next) {
   res.sendFile(path.resolve(__dirname, '../../strive_frontend/dist/index.html'))
 });
 
