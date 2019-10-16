acde4c69d193a18cde24acb8a5cd8329f1d62b49
kaylaTill <kaylatillman319@gmail.com>
2019-10-15 23:44:39 -0700

    Moved NavBar into Seperate Files, Displaying Correctly

--- strive_frontend/src/rootHome.js
+++ strive_frontend/src/rootHome.js
@@ -9,8 +9,8 @@ const UserHomePage = (React.lazy(() => import('./userHomepage.js')));
 const RegisterSuccess = (React.lazy(() => import('./registrationSuccess.js')));
 const RegisterFailure = (React.lazy(() => import('./registrationFailed.js')));
 const LoginFailure = (React.lazy(() => import('./loginFailed.js')));
+const NavBar = (React.lazy(() => import('./nav.js')));
 import axios from 'axios';
-
 
 const Center = styled.h1`
     width: 1000px; 
@@ -21,43 +21,6 @@ const Center = styled.h1`
     font-size: 50px;
     font-family: OCR A Std, monospace;
 `;
-
-const Nav = styled.div`
-    max-width: 1010px;
-    padding: 26px 20px;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    margin: 0 auto;
-    background-color: #fff;
-`;
-const NavLeft = styled.div`
-    width: 20%;
-    text-align: left;
-    color: rgba(0, 0, 0, 1);
-    font-family: OCR A Std, monospace; 
-    text-decoration: none; 
-`;
-
-const NavRight = styled.div`
-    width: 50%;
-    text-align: right;
-    position: relative;
-    left: 45%;
-    svg {
-        margin-right: 50px;
-    }
-`;
-const NavItem = styled.div`
-    color: rgba(0, 0, 0, 1);
-    float: left;
-    padding: 12px;
-    text-decoration: none;
-    text-align: right; 
-    font-size: 15px;
-    font-family: OCR A Std, monospace;
-`;
-
 
 class RootHome extends React.Component {
     constructor(props) {
@@ -158,135 +121,49 @@ class RootHome extends React.Component {
             <Router>
                 {/* REACT ROUTES */}
                 <Suspense fallback={<div></div>}>
+                    {!sessionOpen ? 
+                        <Switch>
+                            {/* HOME PAGE AND LOGOUT  */}
+                            <Route exact={true} path={'/'}>
+                                <NavBar/>
+                                <Center>"Not your average to-do list"</Center>
+                            </Route>
 
-                    {!sessionOpen ? <Switch>
-                        {/* HOME PAGE AND LOGOUT  */}
-                        <Route exact={true} path={'/'}>
-                            <Nav>
-                                <NavLeft>
-                                    <Link to={'/'}>
-                                        <NavItem> Strive </NavItem>
-                                    </Link>
-                                </NavLeft>
+                            {/* ABOUT  */}
+                            <Route exact={true} path={'/about'}>
+                               <NavBar/>
+                                <AboutPage />
+                            </Route>
 
-                                <NavRight>
-                                    <Link to={'/about'}>
-                                        <NavItem> About Strive </NavItem>
-                                    </Link>
-                                    <NavItem>|</NavItem>
-
-                                    <Link to={'/register'}>
-                                        <NavItem> Register </NavItem>
-                                    </Link>
-                                    <NavItem>|</NavItem>
-
-                                    <Link to={'/login'}>
-                                        <NavItem> Login </NavItem>
-                                    </Link>
-                                </NavRight>
-                            </Nav>
-                            <Center>"Not your average to-do list"</Center>
-                        </Route>
-
-                        {/* ABOUT  */}
-                        <Route exact={true} path={'/about'}>
-                            <Nav>
-                                <NavLeft>
-                                    <Link to={'/'}>
-                                        <NavItem> Strive </NavItem>
-                                    </Link>
-                                </NavLeft>
-
-                                <NavRight>
-                                    <Link to={'/about'}>
-                                        <NavItem> About Strive </NavItem>
-                                    </Link>
-                                    <NavItem>|</NavItem>
-
-                                    <Link to={'/register'}>
-                                        <NavItem> Register </NavItem>
-                                    </Link>
-                                    <NavItem>|</NavItem>
-
-                                    <Link to={'/login'}>
-                                        <NavItem> Login </NavItem>
-                                    </Link>
-                                </NavRight>
-                            </Nav>
-                            <AboutPage />
-                        </Route>
-
-                        {/* REGISTER AND LOG INCONFIRMATION */}
-                        <Route exact={true} path={'/registrationSuccess'}>
-                            <RegisterSuccess />
-                        </Route>
-                        <Route exact={true} path={'/registrationFailure'}>
-                            <RegisterFailure />
-                        </Route>
-                        <Route exact={true} path={'/failedLogin'}>
-                            <LoginFailure />
-                        </Route>
+                            {/* REGISTER AND LOG INCONFIRMATION */}
+                            <Route exact={true} path={'/registrationSuccess'}>
+                                <RegisterSuccess />
+                            </Route>
+                            <Route exact={true} path={'/registrationFailure'}>
+                                <RegisterFailure />
+                            </Route>
+                            <Route exact={true} path={'/failedLogin'}>
+                                <LoginFailure />
+                            </Route>
 
 
-                        {/* REGISTER */}
-                        <Route exact={true} path={'/register'}>
-                            <Nav>
-                                <NavLeft>
-                                    <Link to={'/'}>
-                                        <NavItem> Strive </NavItem>
-                                    </Link>
-                                </NavLeft>
+                            {/* REGISTER */}
+                            <Route exact={true} path={'/register'}>
+                               <NavBar/>
+                                <Register handleRegister={this.handleRegister} />
+                                {(redirectSuccess && !redirectFailure) && (<Redirect to={'/registrationSuccess'} />)}
+                                {(!redirectSuccess && redirectFailure) && (<Redirect to={'/registrationFailure'} />)}
+                            </Route>
 
-                                <NavRight>
-                                    <Link to={'/about'}>
-                                        <NavItem> About Strive </NavItem>
-                                    </Link>
-                                    <NavItem>|</NavItem>
-
-                                    <Link to={'/register'}>
-                                        <NavItem> Register </NavItem>
-                                    </Link>
-                                    <NavItem>|</NavItem>
-
-                                    <Link to={'/login'}>
-                                        <NavItem> Login </NavItem>
-                                    </Link>
-                                </NavRight>
-                            </Nav>
-                            <Register handleRegister={this.handleRegister} />
-                            {(redirectSuccess && !redirectFailure) && (<Redirect to={'/registrationSuccess'} />)}
-                            {(!redirectSuccess && redirectFailure) && (<Redirect to={'/registrationFailure'} />)}
-                        </Route>
-
-                        {/* LOGIN */}
-                        <Route exact={true} path={'/login'}>
-                            <Nav>
-                                <NavLeft>
-                                    <Link to={'/'}>
-                                        <NavItem> Strive </NavItem>
-                                    </Link>
-                                </NavLeft>
-
-                                <NavRight>
-                                    <Link to={'/about'}>
-                                        <NavItem> About Strive </NavItem>
-                                    </Link>
-                                    <NavItem>|</NavItem>
-
-                                    <Link to={'/register'}>
-                                        <NavItem> Register </NavItem>
-                                    </Link>
-                                    <NavItem>|</NavItem>
-
-                                    <Link to={'/login'}>
-                                        <NavItem> Login </NavItem>
-                                    </Link>
-                                </NavRight>
-                            </Nav>
-                            <Login handleLogin={this.handleLogin} />
-                            {loginFailed && (<Redirect to={'/failedLogin'} />)}
-                        </Route> </Switch> : <UserHomePage logout={this.handleLogout} sessionStatus={this.state.sessionOpen} />
-                    }
+                            {/* LOGIN */}
+                            <Route exact={true} path={'/login'}>
+                                <NavBar/>
+                                <Login handleLogin={this.handleLogin} />
+                                {loginFailed && (<Redirect to={'/failedLogin'} />)}
+                            </Route> 
+                            </Switch> 
+                            : <UserHomePage logout={this.handleLogout} sessionStatus={this.state.sessionOpen} />
+                        }
                 </Suspense>
             </Router>
         );
