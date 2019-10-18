31542828bd72c6d33268ca681c63382c0dddd714
kaylaTill <kaylatillman319@gmail.com>
2019-10-18 13:08:48 -0700

    Removed Back to Objectives Button

--- strive_frontend/src/moreObjectives.js
+++ strive_frontend/src/moreObjectives.js
@@ -8,24 +8,29 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 const Container = styled.div`
     width: 1150px;
     height: 400px;
+    position: relative;
 `;
 
 const MoreObjectives = ((props) => {
     const allObjectives = props.objectives;
     return (
-        <Container>
-            {allObjectives.map((obj) => (
-                <div>
-                    <Card variant="outline-dark" style={{ width: '20rem', height: '20rem', float: 'left', marginRight: '3rem', overflowX: ' hidden' }}>
-                        <Card.Body style={{ color: '#000000', fontFamily: 'OCR A Std, monospace', textAlign: 'center' }}>
-                            <Card.Title>{obj.name}</Card.Title>
-                            <Card.Text>{obj.description}</Card.Text>
-                            <Card.Link href="/keyResults" style={{ color: '#000000' }}>Key Results</Card.Link>
-                        </Card.Body>
-                    </Card>
-                </div>
-            ))}
-        </Container>
+        <div>
+            <Container>
+                {allObjectives.map((obj) => (
+                    <div>
+                        <Card variant="outline-dark" style={{ width: '20rem', height: '20rem', float: 'left', marginTop: '3rem', 
+                            marginRight: '3rem'}}>
+                            <Card.Body style={{ color: '#000000', fontFamily: 'OCR A Std, monospace', textAlign: 'center' }}>
+                                <Card.Title>{obj.name}</Card.Title>
+                                <Card.Text>{obj.description}</Card.Text>
+                                <Card.Link href="/keyResults" style={{ color: '#000000' }}>Key Results</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                ))}
+            </Container>
+
+        </div>
     );
 });
 
