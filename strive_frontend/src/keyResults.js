760fab272682f4fa63af25f60acc5e7f935a9946
kaylaTill <kaylatillman319@gmail.com>
2019-10-18 14:21:58 -0700

    Styled Key Results page

--- strive_frontend/src/keyResults.js
+++ strive_frontend/src/keyResults.js
@@ -15,23 +15,25 @@ const KeyResults = ((props) => {
             <Container>
                 {objectives.map((obj) => (
                     <div>
-                        <Card variant="outline-dark" style={{ height: '10rem', marginTop: '3rem' }}>
+                        <Card border="dark" style={{ height: '10rem', marginTop: '3rem' }}>
                             <Card.Body style={{ color: '#000000', fontFamily: 'OCR A Std, monospace', textAlign: 'center' }}>
                                 <Card.Title>{obj.name}</Card.Title>
                                 <Card.Text>{obj.description}</Card.Text>
                                 <Card.Text>{obj.timeSpan}</Card.Text>
                             </Card.Body>
                         </Card>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item style={{ color: '#000000', 
-                            fontFamily: 'OCR A Std, monospace', 
-                            marginTop: '3rem', 
-                            textAlign: 'center' }}>Key Results</ListGroup.Item>
-                            <ListGroup.Item>I. {obj.keyResult1}</ListGroup.Item>
-                            <ListGroup.Item>II. {obj.keyResult2}</ListGroup.Item>
-                            <ListGroup.Item>III. {obj.keyResult3}</ListGroup.Item>
-                            <ListGroup.Item>IV. {obj.keyResult4}</ListGroup.Item>
-                            <ListGroup.Item>V. {obj.keyResult5}</ListGroup.Item>
+
+                        <div style={{
+                            color: '#000000',
+                            fontFamily: 'OCR A Std, monospace',
+                            marginTop: '3rem',
+                            textAlign: 'center'}}>Key Results</div>
+                        <ListGroup border="dark" variant="flush">
+                            <ListGroup.Item action border="dark" variant="light">I. {obj.keyResult1}</ListGroup.Item>
+                            <ListGroup.Item action border="dark" variant="light">II. {obj.keyResult2}</ListGroup.Item>
+                            <ListGroup.Item action border="dark" variant="light">III. {obj.keyResult3}</ListGroup.Item>
+                            <ListGroup.Item action border="dark" variant="light">IV. {obj.keyResult4}</ListGroup.Item>
+                            <ListGroup.Item action border="dark" variant="light">V. {obj.keyResult5}</ListGroup.Item>
                         </ListGroup>
                     </div>
                 ))}
